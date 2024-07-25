6e38bcfabf59e0b0843f22f80cee205902e7148f
Abraham Yewa <110853073+yewah-link@users.noreply.github.com>
2024-07-25 15:31:03 +0300

    web update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,17 +2,25 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebook, faInstagram, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faWifi, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faWifi, faEnvelope, faPhone, faBars } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule, CommonModule], // Add CommonModule here
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  mobileMenuVisible = false;
+window: any;
+
   constructor(library: FaIconLibrary) {
-    library.addIcons(faFacebook, faTwitter, faWhatsapp, faInstagram, faWifi, faEnvelope, faPhone);
+    library.addIcons(faFacebook, faTwitter, faWhatsapp, faInstagram, faWifi, faEnvelope, faPhone, faBars);
+  }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuVisible = !this.mobileMenuVisible;
   }
 }
